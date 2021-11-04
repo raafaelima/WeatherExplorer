bdd144e2c39d9d4c367869943aa762fc1a3f57e0
Rafael Lima <Rafael.LA.Lima@ctw.bmwgroup.com>
2021-11-04 23:29:46 +0000

    Add Map with GeoLocation To details screen

--- WeatherExplorer/LocationDetails/LocationDetailViewController.swift
+++ WeatherExplorer/LocationDetails/LocationDetailViewController.swift
@@ -14,6 +14,7 @@ class LocationDetailViewController: UIViewController, Storyboarded {
     @IBOutlet weak var feelsLikeLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var weatherStatusLabel: UILabel!
+    @IBOutlet weak var map: MKMapView!
 
     internal var location: Location?
     weak var coordinator: MainCoordinator?
@@ -35,5 +36,22 @@ class LocationDetailViewController: UIViewController, Storyboarded {
         feelsLikeLabel.text = feelsLikeDescriptor + locationWeather.weather.feelsLikeWithIndicator()
 
         weatherStatusLabel.text = locationWeather.weather.descriptions.first ?? ""
+
+        map.layer.cornerRadius = 10
+        centerMap(on: locationWeather)
+    }
+
+    private func centerMap(on location: Location) {
+        let initialLocation = CLLocation(latitude: location.latitude, longitude: location.longitude)
+        let coordinateRegion = MKCoordinateRegion(center: initialLocation.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        map.setRegion(coordinateRegion, animated: true)
+        addMapPin(on: location)
+    }
+
+    private func addMapPin(on location: Location) {
+        let annotation = MKPointAnnotation()
+        annotation.title = "\(location.name), \(location.region) - \(location.country)"
+        annotation.coordinate = CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
+        map.addAnnotation(annotation)
     }
 }
